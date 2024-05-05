13e4e07683ef80ac1216d16c24c39aab80e37cea
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-05-05 14:16:08 +0330

    feat: add style types

---
+++ src/types/StyleTypes.ts
@@ -0,0 +1,25 @@
+export type TextAlign =
+  | "left"
+  | "center"
+  | "right"
+  | "end"
+  | "justify"
+  | "inherit"
+  | "initial"
+  | "start"
+  | "unset"
+  | "match-parent"
+  | "revert"
+  | "revert-layer";
+
+export type Size = { xl?: string; lg?: string; md?: string; xs?: string };
+export type FontWeight =
+  | "bold"
+  | "light"
+  | "regular"
+  | "medium"
+  | 100
+  | 200
+  | 300
+  | 400
+  | 500;

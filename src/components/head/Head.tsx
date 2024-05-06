962d86451ea6640c21f135359ec27c2bd459224a
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-05-06 12:25:08 +0330

    feat: update Head

--- src/components/head/Head.tsx
+++ src/components/head/Head.tsx
@@ -1,3 +1,39 @@
+import { Box, Typography } from "@mui/material";
+import Title from "../Title";
+import Button from "../Button";
+
 export default function Head() {
-  return <div>Head</div>;
+  return (
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        width="20%"
+      >
+        <Typography>Home</Typography>
+        <Typography>About</Typography>
+        <Typography>Contact</Typography>
+      </Box>
+      <Box>
+        <Title
+          title="Landing"
+          color="#37447E"
+          fontWeight={"bold"}
+          fontSize={"1.5rem"}
+        />
+      </Box>
+      <Box>
+        <Button
+          Title="Buy Now"
+          border={0}
+          bgcolor={"#111B47"}
+          borderRadius={"5px"}
+          color={"#FFFFFF"}
+          width={"12rem"}
+          py={"6px"}
+        />
+      </Box>
+    </Box>
+  );
 }

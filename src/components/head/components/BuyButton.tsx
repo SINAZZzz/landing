30b49ffe64a2a40ae877080748398ac460d37b69
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-05-08 15:22:58 +0330

    feat: Head componentization.

---
+++ src/components/head/components/BuyButton.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+import Button from "../../Button";
+
+export default function BuyButton() {
+  return (
+    <Box>
+      <Button
+        Title="Buy Now"
+        border={0}
+        bgcolor={"#111B47"}
+        borderRadius={"5px"}
+        color={"#FFFFFF"}
+        width={"12rem"}
+        py={"6px"}
+      />
+    </Box>
+  );
+}

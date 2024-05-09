652bdc9ece9abd53e20fddf8f79c6e0075b3ece1
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-05-09 14:31:30 +0330

    feat: update body, completed body

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import Head from "./components/head/Head";
 import Footer from "./components/footer/Footer";
 import Body from "./components/body/Body";
+import Image from "./assets/images/designer_1.png";
 
 function App() {
   return (
     <>
       <Box>
-        {/* head */}
-        <Head />
-        {/* body */}
-        <Body />
-        {/* footer */}
-        <Footer />
+        <Box
+          component="img"
+          src={Image}
+          zIndex={0}
+          position="absolute"
+          right="0"
+          top={0}
+        />
+        <Box zIndex={2} position="relative">
+          <Container>
+            <Head />
+            <Body />
+            <Footer />
+          </Container>
+        </Box>
       </Box>
     </>
   );

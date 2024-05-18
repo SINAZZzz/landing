7906c68346248cc94c93ba4d0774e31bac24db82
Dragon <pardissoheilipenelope@gmail.com>
2024-05-18 13:37:18 -0700

    part3 Body image and text added

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,11 +1,12 @@
 import { Box, Typography, Grid } from "@mui/material";
 import Button from "../Button";
 import Icon from "../../assets/Icon.svg";
+import designer from '../../assets/images/designer_3.png'
 
 export default function Body() {
   return (
     <Box
-      height="140vh"
+      height="160vh"
       display="flex"
       flexDirection="column"
       alignItems="start"
@@ -66,14 +67,39 @@ export default function Body() {
         <Grid container sx={{ width:"40vh", height:"40vh"}}>
             <Grid item md={6} xs={8}><img src={Icon}/>
                 <Typography  variant="h1" fontSize="1rem" fontWeight="bold" color="#091133" my="1rem">Title goes here</Typography>
-                <Typography fontSize="0.8rem" fontWeight="light" color="#091133" my="1rem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorum esse numquam!</Typography>
+                <Typography fontSize="1rem" fontWeight="light" color="#505F98" my="1rem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorum esse numquam!</Typography>
             </Grid>
             <Grid item md={6} xs={8}><img src={Icon}/>
-                <Typography variant="h1" fontSize="1rem" fontWeight="bold" color="#091133" my="1rem">Title goes here</Typography>
-                <Typography fontSize="0.8rem" fontWeight="light" color="#091133" my="1rem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, natus? Quae, laboriosam.</Typography>
+                <Typography variant="h1" fontSize="1rem" fontWeight="bold" color="#505F98" my="1rem">Title goes here</Typography>
+                <Typography fontSize="1rem" fontWeight="light" color="#505F98" my="1rem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, natus? Quae, laboriosam.</Typography>
             </Grid>
         </Grid>
       </Box>
+
+      <Box 
+      display="flex" 
+      justifyContent="space-between"
+      height="20vh"
+      gap="150px"
+      >
+      <Box>
+        <img src={designer} />
+      </Box>
+      <Box>
+        <Box p={2}>
+          <Box p={2}>
+            <Typography variant="h1" fontSize="3rem" fontWeight="bold" color="#091133" mr="1rem" > Light, Fast & Powerfull </Typography>
+            <Typography 
+            fontSize="1rem" fontWeight="light" color="#505F98" my="1rem" mb="2rem">
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br/>Qui dolor corrupti est cumque ducimus animi 
+            <br/>praesentium itaque amet minus nisi delectus obcaecati<br/><br/> explicabo officia, illumneque dicta aperiam iusto sint?<br/>
+            Lorem ipsum dolor sit amet consectetur adipisicing
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </Box>
+        
+  </Box>
   );
 }
